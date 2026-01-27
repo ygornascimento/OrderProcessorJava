624fffda2de98d75948a44e5725e8bcf1f0036ed
Ygor Nascimento <ygor.nascimento@outlook.com>
2026-01-27 18:15:51 -0300

    Esqueleto de projetos da api e worker

---
+++ Orders-Worker/src/main/java/br/tec/itlabs/orders/worker/OrdersWorkerApplication.java
@@ -0,0 +1,13 @@
+package br.tec.itlabs.orders.worker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdersWorkerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrdersWorkerApplication.class, args);
+    }
+
+}
---
+++ Orders-Worker/src/test/java/br/tec/itlabs/orders/worker/OrdersWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.tec.itlabs.orders.worker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersWorkerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
