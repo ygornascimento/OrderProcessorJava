1025f2636503397eb039bded3ccb3906be466865
Ygor Nascimento <ygor.nascimento@outlook.com>
2026-01-29 15:36:58 -0300

    Added CreateOrderRequest DTO

---
+++ Orders-Api/src/main/java/br/tec/itlabs/orders/api/dto/CreateOrderRequest.java
@@ -0,0 +1,44 @@
+package br.tec.itlabs.orders.api.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+public class CreateOrderRequest {
+
+    @NotBlank
+    private String customerName;
+
+    @NotNull
+    @Positive
+    private BigDecimal amount;
+
+    private Instant orderDate;
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public Instant getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(Instant orderDate) {
+        this.orderDate = orderDate;
+    }
+}
