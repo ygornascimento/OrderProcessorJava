bab8fe2d054893ce0f075cc8ac790e5b66fdc785
Ygor Nascimento <ygor.nascimento@outlook.com>
2026-01-29 15:23:01 -0300

    Added Kafka Configuration for Producer

--- Orders-Api/src/main/java/br/tec/itlabs/orders/api/config/KafkaProducerConfig.java
+++ Orders-Api/src/main/java/br/tec/itlabs/orders/api/config/KafkaProducerConfig.java
@@ -1,4 +1,34 @@
 package br.tec.itlabs.orders.api.config;
 
+import br.tec.itlabs.orders.worker.dto.OrderCreatedEvent;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.*;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
 public class KafkaProducerConfig {
+
+    @Bean
+    public ProducerFactory<String, OrderCreatedEvent> producerFactory(KafkaProperties properties) {
+
+        Map<String, Object> props = new HashMap<>(properties.buildProducerProperties());
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+
+        props.put(JsonSerializer.ADD_TYPE_INFO_HEADERS, false);
+
+        return new DefaultKafkaProducerFactory<>(props);
+    }
+
+    @Bean
+    public KafkaTemplate<String, OrderCreatedEvent> kafkaTemplate(ProducerFactory<String, OrderCreatedEvent> producerFactory) {
+        return  new KafkaTemplate<>(producerFactory);
+    }
 }
--- Orders-Api/src/main/java/br/tec/itlabs/orders/api/messaging/OrdersProducer.java
+++ Orders-Api/src/main/java/br/tec/itlabs/orders/api/messaging/OrdersProducer.java
@@ -1,4 +1,23 @@
 package br.tec.itlabs.orders.api.messaging;
 
+import br.tec.itlabs.orders.worker.dto.OrderCreatedEvent;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
 public class OrdersProducer {
+
+    private final KafkaTemplate<String, OrderCreatedEvent> kafkaTemplate;
+    private final String topic;
+
+    public OrdersProducer(KafkaTemplate<String, OrderCreatedEvent> kafkaTemplate,
+                          @Value("${app.kafka.topics.order-created}") String topic) {
+        this.kafkaTemplate = kafkaTemplate;
+        this.topic = topic;
+    }
+
+    public void publishOrderCreated(OrderCreatedEvent event) {
+        kafkaTemplate.send(topic, event.getId(), event);
+    }
 }
